9f3ad7469536e93d82172164d50d77eaa8172371
Chris Duong <chris.duong83@gmail.com>
2014-11-25 17:42:31 +0700

    update json file

--- chef-solo-entrypoint.sh
+++ chef-solo-entrypoint.sh
@@ -7,5 +7,5 @@ cd /chef
 /opt/chef/embedded/bin/berks vendor /etc/chef/cookbooks
 
 if [ -e /chef/solo.json ] && [ -e /chef/solo.rb ]; then
-	exec chef-solo -c solo.rb -j solo.json
+	exec chef-solo -c /chef/solo.rb -j /chef/solo.json
 fi
